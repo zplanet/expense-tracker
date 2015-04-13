1c14fef4706c878780ce994f5c119361dbc8c975
jonghoon kim <infoset@gmail.com>
2015-04-13 17:34:08 -0400

    working on db

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -26,7 +26,6 @@ object Application extends Controller {
 		//Logger.info(Play.current.configuration.getString("db.default.url").get)
 		//Logger.info(Play.current.configuration.getString("db.default.user").get)
 		//Logger.info(Play.current.configuration.getString("db.default.password").get)
-		Logger.info("evolutionplugin: " + Play.current.configuration.getString("evolutionplugin").get)
 		Logger.info("++++++++++++++++++++++++++++++++++++++++++++++")
 
 		users += User("abc@abc.com", "a")

7e7cdc98690874e733f343fd2004ff2d4890b320
jonghoon kim <infoset@gmail.com>
2015-04-13 21:27:19 -0400

    working on db

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,8 @@ import play.api._
 import play.api.mvc._
 
 import play.api.db.slick._
-import play.api.db.slick.Config.driver.simple._
+//import play.api.db.slick.Config.driver.simple._
+import scala.slick.driver.PostgresDriver.simple._
 import models._
 
 object Application extends Controller {
